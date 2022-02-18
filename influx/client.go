87406639651aa8bf8ef308b68d2d100f415a5ded
navy <navy@thingple.com>
2022-02-18 13:51:17 +0800

    up influx/client

--- influx/client.go
+++ influx/client.go
@@ -1,18 +1,18 @@
 package influx
 
 import (
-	"github.com/influxdata/influxdb1-client/v2"
+	"github.com/influxdata/influxdb-client-go/v2"
 	"time"
 )
 
 type Connector struct {
-	proxy client.Client
+	proxy influxdb2.Client
 }
 
-func New(host string) (connector *Connector, err error) {
+func New(host, token string) (connector *Connector, err error) {
 
-	var c client.Client
-	c, err = client.NewHTTPClient(client.HTTPConfig{Addr: host})
+	var c influxdb2.Client
+	c = influxdb2.NewClient(host, token)
 	if err != nil {
 		return connector, err
 	}
@@ -23,26 +23,18 @@ func New(host string) (connector *Connector, err error) {
 }
 
 func (connector Connector) Save(database string, measurement string, tags map[string]string, fields map[string]interface{}) (err error) {
+	// get non-blocking write influxdb2
+	writeAPI := connector.proxy.WriteAPI("tp", database)
+	p := influxdb2.NewPoint(measurement, tags, fields, time.Now())
+	writeAPI.WritePoint(p)
+	// Flush writes
+	writeAPI.Flush()
 
-	var bp client.BatchPoints
-	var point *client.Point
-	bp, err = client.NewBatchPoints(client.BatchPointsConfig{Database: database})
-	if err != nil {
-		return err
-	}
-
-	point, err = client.NewPoint(measurement, tags, fields, time.Now())
-	if err != nil {
-		return err
-	}
-
-	bp.AddPoint(point)
-	err = connector.proxy.Write(bp)
 	return err
 }
 
 func (connector Connector) Close() {
 	if connector.proxy != nil {
-		_ = connector.proxy.Close()
+		connector.proxy.Close()
 	}
 }
